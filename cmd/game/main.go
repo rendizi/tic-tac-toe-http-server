b4ddbecf6055258137ff1013e09d91bf7683c800
Baglanov Alikhan <120701599+rendizi@users.noreply.github.com>
2023-12-12 11:44:39 +0500

    Update main.go
    Configured listen and serve

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/aivanov/game/http/server/handler"
+	"github.com/rendizi/tic-tac-toe-http-server/http/server/handler"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.HandleFunc("/newgame", handler.NewGame)
+	http.HandleFunc("/game", handler.Game)
 	err := http.ListenAndServe(":3333", nil)
 	if err != nil {
 		if err == http.ErrServerClosed {
